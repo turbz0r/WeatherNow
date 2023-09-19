b38e44c3001f262d5360c2be46b4459dd104398d
turb <turbzor@gmail.com>
2023-09-19 18:44:43 +0300

    different styling changes/animations + input check

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
             );
             return response;
         } catch (error) {
-            throw new Error(`Error ocurred.\n Could not fetch data.\n Status: ${response.status}`);
+            throw new Error(`Error ocurred.\n Could not fetch data.\n Status: ${error.response.status}`);
         }
     }
 
@@ -74,31 +74,21 @@ window.addEventListener('DOMContentLoaded', () => {
         return `${string[0].toUpperCase()}${string.slice(1)}`;
     }
 
-    searchBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        if (searchInput.value.length < 2) {
-            return false;
+    window.addEventListener('click', (event) => {
+        if (event.target === searchInput) {
+            searchForm.style.border = '1px solid #22a39f';
+        } else {
+            searchForm.style.border = '1px solid transparent';
         }
-
-        getWeather(fixedSearchValue(searchInput.value)).then((response) => {
-            new WeatherCard(
-                response.data.name,
-                response.data.main.temp,
-                response.data.weather[0].icon,
-                response.data.weather[0].description,
-                response.data.main.humidity,
-                response.data.main.pressure,
-                response.data.wind.speed
-            ).appendCard();
-        });
     });
 
     //weather output (buttons and output field itself)
 
     const weatherButtons = document.querySelectorAll('.weather-button'),
+        weatherButtonsWrapper = document.querySelector('.weather-buttons__wrapper'),
         weatherOutput = document.querySelector('.weather-output'),
-        weatherWrapper = document.querySelector('.weather-output__wrapper');
+        weatherWrapper = document.querySelector('.weather-output__wrapper'),
+        errorInput = document.querySelector('.input-error');
 
     function buttonsReset() {
         weatherButtons.forEach((item) => {
@@ -113,5 +103,55 @@ window.addEventListener('DOMContentLoaded', () => {
             buttonsReset();
             event.target.classList.add('button-active');
         }
+    });
+
+    weatherButtonsWrapper.addEventListener('click', (event) => {
+        if (event.target.id === 'btn-fivedays') {
+            weatherOutput.classList.add('slide-out');
+            setTimeout(() => {
+                weatherOutput.classList.add('slide-in');
+            }, 20);
+            // weather panel reappear animation TODO
+        }
+    });
+
+    searchBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        errorInput.style.display = 'none';
+
+        if (searchInput.value.length < 1 || searchInput.value.trim().length === 0) {
+            return false;
+        }
+
+        if (weatherOutput.dataset.activePlace === searchInput.value.toLowerCase()) {
+            return false;
+        }
+
+        getWeather(fixedSearchValue(searchInput.value))
+            .then((response) => {
+                if (!weatherWrapper.classList.contains('weather-acitve')) {
+                    weatherWrapper.classList.add('weather-active');
+                }
+                weatherOutput.innerHTML = '';
+
+                weatherOutput.dataset.activePlace = response.data.name.toLowerCase();
+
+                new WeatherCard(
+                    response.data.name,
+                    response.data.main.temp,
+                    response.data.weather[0].icon,
+                    response.data.weather[0].description,
+                    response.data.main.humidity,
+                    response.data.main.pressure,
+                    response.data.wind.speed
+                ).appendCard();
+            })
+            .catch((err) => {
+                if (err.toString().includes('404')) {
+                    errorInput.style.display = 'block';
+                    searchForm.style.border = '1px solid red';
+                }
+                console.error(err);
+            });
     });
 });
